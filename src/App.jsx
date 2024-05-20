4aac9bf27571fb4e645e61e4683fc02ef152b3a1
JAmal Michael <eshimi090@gmail.com>
2024-05-19 20:21:15 -1200

    creating the signup section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Signup from "./sections/auth/Signup"
 
 
 function App() {
@@ -5,9 +6,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <Signup />
     </>
   )
 }
